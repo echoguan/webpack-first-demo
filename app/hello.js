276d54e916fdcbbc228ad94cdca372ecf511a95d
echo.guan <echo.guan@perficient.com>
2018-03-14 13:35:26 +0800

    Initial commit

---
+++ app/hello.js
@@ -0,0 +1,6 @@
+import "./world.js";
+import "css-loader!./style.css";
+
+function hello(str) {
+  alert(str);
+}
---
+++ app/main.js
@@ -0,0 +1,4 @@
+import greet from "./Greeter.js";
+
+const greeter = greet;
+document.querySelector("#root").appendChild(greeter());
