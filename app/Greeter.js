ae355556056bf97aec4cdceffabab825d421d2d4
echo.guan <echo.guan@perficient.com>
2018-03-14 13:35:53 +0800

    Greeter.js

--- app/Greeter.js
+++ app/Greeter.js
@@ -1,1 +1,11 @@
+// export const greet = function() {
+//   let greet = document.createElement("div");
+//   greet.textContent = "Hello, my first webpack app!";
+//   return greet;
+// };
 
+module.exports = function() {
+  var greet = document.createElement("div");
+  greet.textContent = "Hi there and greetings!";
+  return greet;
+};
